ca9c97640436c63cb8c22ea1f31e72e166738b9a
Dibakar Sutra Dhar <crossbonesdipu@gmail.com>
2017-03-25 16:13:14 +0800

    Completed

---
+++ Equip.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Equip : AbstractBehavior {
+
+	private int _currentItem = 0;
+	private Animator animatior;
+
+	public int currentItem{
+		get { return _currentItem; }
+		set{
+			_currentItem = value;
+			animatior.SetInteger ("EquippedItem", _currentItem);
+		}
+	}
+
+	override protected void Awake(){
+		base.Awake ();
+		animatior = GetComponent<Animator> ();
+	}
+}
